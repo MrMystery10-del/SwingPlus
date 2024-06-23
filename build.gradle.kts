d4a52fbf0000265fc205fc1c5cdc9b5abd3ea3f1
Mr Mystery 1.0 <69641472+MrMystery10-del@users.noreply.github.com>
2024-06-23 13:02:46 +0200

    Publishing configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,42 @@
+import org.gradle.api.publish.PublishingExtension
+
 plugins {
     id("java")
     id("io.freefair.lombok") version "8.6"
+    `maven-publish`
 }
 
 group = "com.swingplus"
 version = "1.0-SNAPSHOT"
+
+configure<PublishingExtension> {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = "com.swingplus"
+            artifactId = "swingplus"
+            version = "0.0.1"
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/MrMystery10-del/SwingPlus")
+            credentials {
+                val githubUsername: String? = findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+                val githubToken: String? = findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+
+                print("Username:" + System.getenv("GITHUB_USERNAME"));
+                if (githubUsername == null || githubToken == null) {
+                    throw GradleException("GitHub username or token is not set. Please set the GITHUB_USERNAME and GITHUB_TOKEN environment variables.")
+                }
+
+                username = githubUsername
+                password = githubToken
+            }
+        }
+    }
+}
 
 repositories {
     mavenCentral()
